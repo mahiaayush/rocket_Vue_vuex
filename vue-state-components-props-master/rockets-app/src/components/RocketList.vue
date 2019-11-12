ae6545a7bca42875fbef38318566ba5ee13d04c5
ajeet <mahi.aayush@gmail.com>
2019-11-12 12:15:13 +0530

    reading vue and vuex

---
+++ vue-state-components-props-master/rockets-app/src/components/RocketList.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <section class="row">
+    <rocket
+      v-for="rocket in rockets"
+      :key="rocket.name"
+      :rocket="rocket">
+    </rocket>
+  </section>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+import Rocket from '@/components/Rocket';
+
+export default {
+  name: 'rocket-list',
+  props: ['rockets'],
+  computed: mapGetters(['rockets']),
+  components: {
+    Rocket,
+  },
+};
+</script>
---
+++ vue-state-components-props-master/rockets-app/src/views/Rockets.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+  <section>
+    <new-rocket-form>
+    </new-rocket-form>
+    <rocket-list>
+    </rocket-list>
+  </section>
+</template>
+
+<script>
+import NewRocketForm from '@/components/NewRocketForm';
+import RocketList from '@/components/RocketList';
+
+export default {
+  name: 'rockets',
+  components: {
+    NewRocketForm,
+    RocketList,
+  },
+  async mounted() {
+    this.$store.dispatch('getRockets');
+  }
+};
+</script>
+
+<style lang="css">
+</style>
